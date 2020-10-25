28beeaf8984b02ba4c7a668db83fff4f4ef08850
vagrant <vagrant@homestead>
2020-10-25 12:00:42 +0000

    添加驗證碼

---
+++ database/migrations/2020_10_25_163535_seed_categories_data.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SeedCategoriesData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $categories = [
+            [
+                'name'        => '分享',
+                'description' => '分享創造，分享發現',
+            ],
+            [
+                'name'        => '教程',
+                'description' => '開發技巧、推薦插件等',
+            ],
+            [
+                'name'        => '問答',
+                'description' => '請保持友善，互幫互助',
+            ],
+            [
+                'name'        => '公告',
+                'description' => '站點公告',
+            ],
+        ];
+
+        DB::table('categories')->insert($categories);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('categories')->truncate();
+    }
+}
