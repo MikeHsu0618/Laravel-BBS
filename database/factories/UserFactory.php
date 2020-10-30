778981a15477d36711d6382b10ef7bc011dd8a60
vagrant <vagrant@homestead>
2020-10-30 08:35:55 +0000

    commit 測試

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,6 +16,16 @@ use Illuminate\Support\Str;
 | model instances for testing / seeding your application's database.
 |
 */
+$avatars = [
+    'https://cdn.learnku.com/uploads/images/201710/14/1/s5ehp11z6s.png',
+    'https://cdn.learnku.com/uploads/images/201710/14/1/Lhd1SHqu86.png',
+    'https://cdn.learnku.com/uploads/images/201710/14/1/LOnMrqbHJn.png',
+    'https://cdn.learnku.com/uploads/images/201710/14/1/xAuDMxteQy.png',
+    'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png',
+    'https://cdn.learnku.com/uploads/images/201710/14/1/NDnzMutoxX.png',
+];
+
+
 
 $factory->define(User::class, function (Faker $faker) {
     return [
