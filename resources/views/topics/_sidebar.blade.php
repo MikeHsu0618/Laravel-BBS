b9446c764dccad272050b9dfdfb4b175c4a0f29a
vagrant <vagrant@homestead>
2020-11-01 06:57:44 +0000

    用戶可以創建話題

--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,5 +1,7 @@
 <div class="card ">
-    <div class="card-body">
-      右邊導覽列
-    </div>
+  <div class="card-body">
+    <a href="{{ route('topics.create') }}" class="btn btn-success btn-block" aria-label="Left Align">
+      <i class="fas fa-pencil-alt mr-2"></i>  新建文章
+    </a>
   </div>
+</div>
