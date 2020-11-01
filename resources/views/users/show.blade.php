84d7f07fa0d1b17aba61e679039f7986b1bff95a
vagrant <vagrant@homestead>
2020-11-01 06:26:51 +0000

    用戶話題列表

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -27,10 +27,15 @@
     <hr>
 
     {{-- 用户发布的内容 --}}
-    <div class="card ">
+    <div class="card">
       <div class="card-body">
-        暂无数据 ~_~
+        <ul class="nav nav-tabs">
+          <li class="nav-item"><a class="nav-link active bg-transparent" href="#">Ta 的話題</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Ta 的回覆</a></li>
+        </ul>
+        @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
       </div>
+    </div>
     </div>
 
   </div>
