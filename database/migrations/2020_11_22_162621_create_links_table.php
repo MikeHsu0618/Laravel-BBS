fd5263deaba1f360f125cf37e2402dd18abf5dc9
vagrant <vagrant@homestead>
2020-11-22 08:51:05 +0000

    側邊欄連結

---
+++ database/migrations/2020_11_22_162621_create_links_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->comment('資源的描述')->index();
+            $table->string('link')->comment('資源的連結')->index();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
